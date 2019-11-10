dc863fb04737aed5dee0ccfb8ab0ef8644cc7de3
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2019-11-10 14:45:23 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,57 +1,92 @@
 package InvoicePackage;
 
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.File;
 import java.net.MalformedURLException;
+import java.net.URL;
 
 
-public class HTTP {
-	
-	private static HttpURLConnection connection;
-	
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.events.EventListener;
+import org.w3c.dom.events.EventTarget;
+import org.w3c.dom.html.HTMLDocument;
+import org.w3c.dom.html.HTMLElement;
+
+//import org.w3c.dom.Document;
+//import org.w3c.dom.Element;
+//import org.w3c.dom.events.*;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.Event;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.geometry.Insets;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Main extends Application{
 	public static void main(String[] args) {
-		BufferedReader reader;
-		String line;
-		StringBuffer responseContent = new StringBuffer();
-		try {
-			URL url = new URL("https://jsonplaceholder.typicode.com/albums");
-			connection = (HttpURLConnection) url.openConnection();
-			
-			//Request setup
-			connection.setRequestMethod("GET");
-			connection.setConnectTimeout(5000);
-			connection.setReadTimeout(5000);
-			
-			int status = connection.getResponseCode();
-			//System.out.println(status);
-			
-			if (status > 299) {
-				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-				while ((line = reader.readLine()) != null) {
-					responseContent.append(line);
-				}
-				reader.close();
-			}
-			else {
-				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-				while ((line = reader.readLine()) != null) {
-					responseContent.append(line);
-				}
-				reader.close();
-			}
-			System.out.println(responseContent.toString());
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		finally {
-			connection.disconnect();
-		}
+		launch(args);
 	}
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Invoice");
+		VBox pane = new VBox(1);
+		pane.setPadding(new Insets(15, 5, 5, 5));
+		//file:///C:/Users/denni/Desktop/Desktop/CSC205/CSC205-Lab07/Lab07
+		
+		URL url = this.getClass().getResource("/MyForm.html");
+		
+		
+		pane.getChildren().addAll(new Label("Shirts"), new TextField());
+		pane.getChildren().addAll(new Label("Pants"), new TextField());
+		pane.getChildren().addAll(new Label("Ties"), new TextField());
+		pane.getChildren().addAll(new Label("Shoes"), new TextField());
+		
+		final WebView browser = new WebView();
+		//final WebEngine webEngine = browser.getEngine();
+		WebEngine webEngine = browser.getEngine();
+		System.out.print(url.toString());
+		webEngine.load(url.toString());
+	
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.getStyleClass().add("noborder-scroll-pane");
+        scrollPane.setStyle("-fx-background-color: white");
+        scrollPane.setContent(browser);
+        scrollPane.setFitToWidth(true);
+        
+        pane.getChildren().add(scrollPane);
+		
+		
+		Scene scene = new Scene(pane, 200, 250);
+		primaryStage.setScene(scene);;
+		primaryStage.show();
+		
+		EventListener listener = new EventListener() {
+			@Override
+			public void handleEvent(org.w3c.dom.events.Event ev) {
+		        Platform.exit();
+		    }
+		};
+		
+		HTMLDocument doc = (HTMLDocument) webEngine.getDocument();
+		HTMLElement form1 = (HTMLElement) doc.getElementById("submit");
+		((EventTarget) form1).addEventListener("click", listener, false);
+		
+		
+	}
+	
 }
